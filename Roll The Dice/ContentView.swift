95ad04fe62559afc264e011e3034f34d7e5fb16e
Daksh <dnakra@bsd220.org>
2022-12-12 09:53:50 -0600

    Finished Project

--- Roll The Dice/ContentView.swift
+++ Roll The Dice/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Dice Roll")
+                .font(.title)
+                .padding()
+            Image("pips \(randomValue)")
+                .resizable()
+                .frame(width: 200, height: 200, alignment: .center)
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 1))
+                .padding()
+                .onTapGesture {
+                    chooseRandom(times: 3)
+                    withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                        rotation += 360
+                    }
+                }
         }
-        .padding()
+    }
+    func chooseRandom(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
+        }
     }
 }
 
