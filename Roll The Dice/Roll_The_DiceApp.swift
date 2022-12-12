4b798c1b700ead844c458947cc09b33604f31ddf
Daksh <dnakra@bsd220.org>
2022-12-12 09:35:37 -0600

    Initial Commit

---
+++ Roll The Dice/Roll_The_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_The_DiceApp.swift
+//  Roll The Dice
+//
+//  Created by Daksh Nakra on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Roll_The_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
